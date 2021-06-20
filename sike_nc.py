d5f58466c44f05fc90544eac5bad002beb9a3a74
jmilkowski <jmilkowski@i2asolutions.com>
2021-06-20 18:00:51 +0200

    utf-16, exchange confirmation

--- sike_nc.py
+++ sike_nc.py
@@ -3,21 +3,27 @@ import logging
 import socket
 import threading
 import sike
-from enum import IntEnum
 
 from Crypto.Cipher import AES
 
-
 BUFFER_SIZE = 1024
+ENCODING = 'utf-16'
+
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+NEW_LINE = '\n'
+
+PRE_TEXT = NEW_LINE + CURSOR_UP_ONE + ERASE_LINE
+
+EXCHANGE_CONFIRMATION = b'***CONFIRMED_EXCHANGE****'
 
 
-class StatusCodesEnum(IntEnum):
-    SECURE = 9999
-    NOT_SECURE = 1000
+def _print(raw_data):
+    print(PRE_TEXT + '>' + raw_data)
 
 
 def padding(s):
-    return s + ((16 - len(s) % 16) * '~')
+    return s + (((8 - len(s) % 8) - 1) * '~')
 
 
 def remove_padding(s: str):
@@ -32,7 +38,8 @@ class SendMessageBase:
             raw_data = input()
             if raw_data:
                 if self.is_secure:
-                    encrypted_data = aes.encrypt(padding(raw_data).encode("utf16"))
+                    data = padding(raw_data).encode("utf16")
+                    encrypted_data = aes.encrypt(data)
                     logging.debug('Sending encrypted message: \n%s\n key: %s', encrypted_data,
                                   self.key)
                     socket.send(bytes(encrypted_data))
@@ -48,19 +55,21 @@ class Server(SendMessageBase):
         self.key = 'Sixteen byte key'
         self.is_secure = secure
 
-    def establish_secure(self):
-        pass
-
     def key_exchange(self):
         logging.info('Exchanging key...')
         logging.debug('Waiting for public key response...')
         public_key = self.connection.recv(BUFFER_SIZE)
         #logging.debug('Reviced public key: %s', public_key.hex())
-        logging.debug('Encapsulating key...')
+        logging.info('Encapsulating key...')
         shared_secret, ciphertext = sike.encapsulate(public_key)
-        logging.debug('Sending cypher text message...')
+        logging.info('Sending cypher text message...')
         self.connection.sendall(ciphertext)
+        logging.info('Waiting for confirmation...')
+        confirmation = self.connection.recv(BUFFER_SIZE)
+        if not confirmation == EXCHANGE_CONFIRMATION:
+            self.socket.close()
         logging.info('Key exchanged.')
+
         logging.debug('Shared secret key is: %s', shared_secret.hex())
         return shared_secret
 
@@ -71,8 +80,7 @@ class Server(SendMessageBase):
         try:
             self.connection, addr = self.socket.accept()
         except KeyboardInterrupt:
-            self.socket.close()
-            exit(0)
+            raise
 
         with self.connection:
             logging.info('Connected by %s', addr[0])
@@ -89,16 +97,16 @@ class Server(SendMessageBase):
                     raw_data = self.connection.recv(BUFFER_SIZE)
                     if self.is_secure:
                         logging.debug('Received encrypted message: %s', raw_data)
-                        decrypted_data = remove_padding(str(aes.decrypt(raw_data), 'utf-16'))
-                        print('>', decrypted_data)
+                        decrypted_data = remove_padding(str(aes.decrypt(raw_data), ENCODING))
+                        _print(decrypted_data)
                     else:
-                        print('>', str(raw_data, 'utf-16'))
+                        _print(raw_data)
                     if not raw_data:
                         break
             except KeyboardInterrupt:
-                self.socket.close()
-                exit(0)
+                raise
         self.socket.close()
+        print('Connection closed.')
 
 
 class Client(SendMessageBase):
@@ -121,8 +129,9 @@ class Client(SendMessageBase):
         logging.debug('Waiting for cypher text response...')
         cyphertext_message = self.socket.recv(BUFFER_SIZE)
         logging.debug('Recived cypher text message: %s', cyphertext_message.hex())
-        logging.debug('Decapsulating shared key...')
+        logging.info('Decapsulating shared key...')
         shared_secret = sike.decapsulate(secret_key, cyphertext_message)
+        self.socket.send(EXCHANGE_CONFIRMATION)
         logging.info('Key exchanged.')
         logging.debug('Shared secret key is: %s', shared_secret.hex())
         return shared_secret
@@ -145,14 +154,15 @@ class Client(SendMessageBase):
                     break
                 if self.is_secure:
                     logging.debug('Received encrypted message: %s', raw_data.hex())
-                    decrypted_data = remove_padding(str(aes.decrypt(raw_data), 'utf-16'))
-                    print('>', decrypted_data)
+                    decrypted_data = remove_padding(str(aes.decrypt(raw_data), ENCODING))
+                    _print(decrypted_data)
                 else:
-                    print('>', str(raw_data, 'utf-16'))
+                    _print(raw_data)
         except KeyboardInterrupt:
-            self.socket.close()
-            exit(0)
+            raise
+
         self.socket.close()
+        print('Connection closed.')
 
 
 def main():
@@ -160,8 +170,8 @@ def main():
     parser.add_argument("-l", "--listen", help="Listen mode, for inbound connects",
                         action='store_true')
     parser.add_argument("-p", "--server-port", required=False, type=int)
-    parser.add_argument("-s", "--secure", action='store_true')
-    parser.add_argument("--log", type=str, default='DEBUG')
+    parser.add_argument("-ns", "--not-secure", action='store_true')
+    parser.add_argument("--log", type=str, default='INFO')
     parser.add_argument("destination", nargs='?')
     parser.add_argument("port", nargs='?', type=int)
 
@@ -171,21 +181,24 @@ def main():
     if not isinstance(log_level, int):
         raise ValueError('Invalid log level: %s' % args.log)
     logging.basicConfig(level=log_level, format='%(levelname)s:%(message)s')
+    secure = not args.not_secure
 
     if args.listen and args.server_port:
-        server = Server(secure=args.secure)
+        server = Server(secure=secure)
         try:
             server.start(port=args.server_port)
-        except Exception as e:
+        except (Exception, KeyboardInterrupt) as e:
             server.socket.close()
-            raise e
+            print('Connection closed.')
+            raise
     elif args.destination and args.port:
-        client = Client(secure=args.secure)
+        client = Client(secure=secure)
         try:
             client.connect(args.destination, args.port)
-        except Exception as e:
+        except (Exception, KeyboardInterrupt) as e:
             client.socket.close()
-            raise e
+            print('Connection closed.')
+            raise
     else:
         raise Exception()
 
